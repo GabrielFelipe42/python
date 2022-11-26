76825af7b67054809dfc925bcfa60ca08645f608
Gabriel Felipe <gabrielfelipe11102002@gmail.com>
2022-11-26 18:33:40 -0300

    att

--- Nome idade peso.py
+++
@@ -1,5 +0,0 @@
-nome = input('Qual seu nome?: ')
-idade = input('Qual sua idade: ')
-peso = input('Qual seu peso: ')
-
-print ('{} para sua idade ({}) seu peso ({}) é bom'.format(nome,idade,peso) )
--- sexo while.py
+++
@@ -1,4 +0,0 @@
-sexo = str(input('Qual o seu sexo ?[M/F]: ')).strip().upper()
-while sexo not in ['M','F']:
-        sexo = str(input ('Digite uma opção valida: ')).upper().strip()
-print (f'Salvamos o seu sexo como {sexo}')
